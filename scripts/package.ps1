38503c90bdfec595d601cc2bd1cc16e3855bea10
sqeezy <sqeezy@gmx.de>
2015-10-17 23:43:00 +0200

    Allways rebooting after updates.

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -32,7 +32,8 @@ Write-BoxstarterMessage "Removing page file"
 $pageFileMemoryKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management"
 Set-ItemProperty -Path $pageFileMemoryKey -Name PagingFiles -Value ""
 
-if(Test-PendingReboot){ Invoke-Reboot }
+#if(Test-PendingReboot){ Invoke-Reboot }
+Invoke-Reboot
 
 Write-BoxstarterMessage "Cleaning SxS..."
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
