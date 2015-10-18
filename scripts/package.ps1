af8b249644695d72fb5ec74479ca60c8a7600b74
sqeezy <sqeezy@gmx.de>
2015-10-18 13:05:32 +0200

    Back to rebooting when needed.

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -32,8 +32,7 @@ Write-BoxstarterMessage "Removing page file"
 $pageFileMemoryKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management"
 Set-ItemProperty -Path $pageFileMemoryKey -Name PagingFiles -Value ""
 
-#if(Test-PendingReboot){ Invoke-Reboot }
-Invoke-Reboot
+if(Test-PendingReboot){ Invoke-Reboot }
 
 Write-BoxstarterMessage "Cleaning SxS..."
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
